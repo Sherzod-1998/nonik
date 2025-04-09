c2d83494470470e1b480a3174d06737cd7567441
Sherzod-1998 <sherzod.oltinsoy@gmail.com>
2025-04-09 21:06:02 +0900

    prettier

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 /** 2-SESSIONS **/
-
 /** 3-VIEWS **/
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
