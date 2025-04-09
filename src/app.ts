1c611d2bc6dc0d4b51ee1166398652a31cd38d11
Sherzod-1998 <sherzod.oltinsoy@gmail.com>
2025-04-09 20:17:15 +0900

    feat: express integrations

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import path from "path";
+
+//1-ENTRANCE
+const app = express();
+app.use(express.static(path.join(__dirname,"public")));
+
+//2-SESSION
+
+
+
+
+//3-VIEWS
+
+
+
+
+//4-ROUTERS
+
+export default app;
