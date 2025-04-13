1a38b5d79c7778e78886e899c2bef77e7bdb9a46
Sherzod-1998 <sherzod.oltinsoy@gmail.com>
2025-04-13 19:08:23 +0900

    feat: create routing and member

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,19 @@
-import express from "express";
-import path from "path";
-import router from "./router";
+import express from 'express';
+import path from 'path';
+import router from './router';
 
 /** 1-ENTRANCE **/
 const app = express();
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 /** 2-SESSIONS **/
 /** 3-VIEWS **/
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 /** 4-ROUTERS **/
-app.use("/", router);
+app.use('/', router);
 
 export default app;
