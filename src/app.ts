af485d3f182a9c8f099dd1cf8eb23978f85afc13
Sherzod-1998 <sherzod.oltinsoy@gmail.com>
2025-04-22 23:38:47 +0900

    feat: integrate middleware sessions

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,14 @@ import morgan from 'morgan';
 import { MORGAM_FORMAT } from './libs/config';
 import routerAdmin from './router-admin';
 
+import session from "express-session";
+import ConnectMongoDB from "connect-mongodb-session";
+const MongoDBStore = ConnectMongoDB(session);
+const store = new MongoDBStore({
+    uri: String(process.env.MONGO_URL) ,
+    collection: 'sessions'
+  });
+
 /** 1-ENTRANCE **/
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
@@ -13,6 +21,17 @@ app.use(express.json());
 app.use(morgan(MORGAM_FORMAT));
 
 /** 2-SESSIONS **/
+app.use(
+    session({
+      secret: String(process.env.SESSION_SECRET), // secret key
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
+      },
+      store: store, 
+      resave: true, 
+      saveUninitialized: true 
+    })
+  );
 
 /** 3-VIEWS **/
 app.set('views', path.join(__dirname, 'views'));
