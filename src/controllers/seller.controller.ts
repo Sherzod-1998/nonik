48552278be7817adf866f3448336be0dfa10d407
Sherzod-1998 <sherzod.oltinsoy@gmail.com>
2025-04-23 18:56:37 +0900

    fix: create sessions on login and signup process

--- src/controllers/seller.controller.ts
+++ src/controllers/seller.controller.ts
@@ -1,69 +1,67 @@
-import { request, Request, Response } from "express";
-import { T } from "../libs/types/common";
-import MemberService from "../models/Member.service";
-import { LoginInput, MemberInput } from "../libs/types/member";
-import { MemberType } from "../libs/enums/member.enum";
+import { Request, Response } from 'express';
+import { T } from '../libs/types/common';
+import MemberService from '../models/Member.service';
+import { AdminRequest, LoginInput, MemberInput } from '../libs/types/member';
+import { MemberType } from '../libs/enums/member.enum';
 
 const memberService = new MemberService();
 
 const sellerController: T = {};
 sellerController.goHome = (req: Request, res: Response) => {
-  try {
-    console.log("goHome");
-    res.render("home");
-  } catch (err) {
-    console.log("Error, goHome:", err);
-  }
+	try {
+		console.log('goHome');
+		res.render('Home');
+	} catch (err) {
+		console.log('Error, goHome:', err);
+	}
 };
 
 sellerController.getSignup = (req: Request, res: Response) => {
-  try {
-    res.render("Signup");
-  } catch (err) {
-    console.log("Error, getSignup:", err);
-  }
+	try {
+		res.render('signup');
+	} catch (err) {
+		console.log('Error, getSignup:', err);
+	}
 };
-
 
 sellerController.getLogin = (req: Request, res: Response) => {
-  try {
-    res.render("Login");
-  } catch (err) {
-    console.log("Error, getLogin:", err);
-  }
+	try {
+		res.render('Login');
+	} catch (err) {
+		console.log('Error, getLogin:', err);
+	}
 };
 
+sellerController.processSignup = async (req: AdminRequest, res: Response) => {
+	try {
+		console.log('processSignup');
+		const newMember: MemberInput = req.body;
+		newMember.memberType = MemberType.SELLER;
+		const result = await memberService.processSignup(newMember);
 
-
-sellerController.processSignup = async(req: Request, res: Response) => {
-  try {
-    console.log("processSignup");
-    console.log("body:", req.body);
-
-    const newMember:MemberInput = req.body;
-    newMember.memberType = MemberType.SELLER;
-    const result = await memberService.processSignup(newMember);
-    res.send(result);
-  } catch (err) {
-    console.log("Error, processSignup:", err);
-    res.send(err);
-  }
+		req.session.member = result;
+		req.session.save(function () {
+			res.send(result);
+		});
+	} catch (err) {
+		console.log('Error, processSignup:', err);
+		res.send(err);
+	}
 };
 
-sellerController.processLogin = async (req: Request, res: Response) => {
-  try {
-    console.log("processLogin");
-    console.log("body:", req.body);
-    const input:LoginInput = req.body;
-    
-    const result = await memberService.processLogin(input);
+sellerController.processLogin = async (req: AdminRequest, res: Response) => {
+	try {
+		console.log('processLogin');
+		console.log('body:', req.body);
+		const input: LoginInput = req.body;
 
-    res.send(result);
-  } catch (err) {
-    console.log("Error, processLogin:", err);
-    res.send(err);
-  }
+		const result = await memberService.processLogin(input);
+
+		res.send(result);
+	} catch (err) {
+		console.log('Error, processLogin:', err);
+		res.send(err);
+	}
 };
-
 
 export default sellerController;
