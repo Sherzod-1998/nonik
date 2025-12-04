cda7ffbb1f801402f2b5816e0875462d265c8c7b
Sherzod-1998 <sherzod.oltinsoy@gmail.com>
2025-12-04 17:09:11 +0900

    ...

--- src/public/js/users.js
+++ src/public/js/users.js
@@ -1,27 +1,27 @@
-console.log("Users frontend javascript file");
+console.log('Users frontend javascript file');
 
-console.log("Users frontend javascript file");
+console.log('Users frontend javascript file');
 
 $(function () {
-    $(".member-status").on("change", function (e) {
-        const id = e.target.id,
-            memberStatus = $(`#${id}.member-status`).val();
-        axios 
-        .post("/admin/user/edit", {
-            _id: id,
-            memberStatus: memberStatus,
-        })
-        .then((response) => {
-            console.log("response:", response);
-            const result = response.data;
+	$('.member-status').on('change', function (e) {
+		const id = e.target.id,
+			memberStatus = $(`#${id}.member-status`).val();
+		axios
+			.post('/admin/user/edit', {
+				_id: id,
+				memberStatus: memberStatus,
+			})
+			.then((response) => {
+				console.log('response:', response);
+				const result = response.data;
 
-            if (result.data) {
-                $(".member-status").blur();
-            } else alert("User update failed!");
-        })
-        .catch((err) => {
-            console.log(err);
-            alert("User update failed!");
-        });
-    });
+				if (result.data) {
+					$('.member-status').blur();
+				} else alert('User update failed!');
+			})
+			.catch((err) => {
+				console.log(err);
+				alert('User update failed!');
+			});
+	});
 });
