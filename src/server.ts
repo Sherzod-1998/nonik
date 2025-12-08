0290718ddbb40ca1d79124252f4c93eba36258f0
Sherzod-1998 <sherzod.oltinsoy@gmail.com>
2025-12-08 21:21:58 +0900

    feat: prepare for production

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import dotenv from 'dotenv';
-dotenv.config();
+dotenv.config({
+	path: process.env.NODE_ENV === 'production' ? '.env.production' : '.env',
+});
 
 import mongoose from 'mongoose';
 import server from './app';
